a2b0a7ff1603b6dfdf7776026b1cb7a62fc1bb01
gyuri.son <gyuri.son@weaverloft.com>
2024-05-07 16:23:23 +0900

    first commit

---
+++ asset/js/mall-core.js
@@ -0,0 +1,37 @@
+const counter = { counter: 0 };
+
+function countAnimation(selector, value) {
+  const target = document.querySelector(selector);
+  gsap.to(counter, {
+    counter: value,
+    scrollTrigger: {
+      trigger: ".benefit-sec",
+      start: "0% 80%",
+      end: "100% 0%",
+      toggleActions: "play none none none",
+      // markers: true,
+    },
+    onUpdate: () => {
+      target.innerHTML = (value > 0 ? "+" : "") + counter.counter.toFixed() + "<sup>%</sup>";
+    },
+    duration: 2,
+  });
+}
+
+countAnimation("#build-cost", -50);
+countAnimation("#operation-cost", -70);
+countAnimation("#speed", 200);
+
+$(".pricing-sec .btn-more").on("click", function () {
+  const element = $(".pricing-sec .hide");
+  element.toggle();
+  if (element.css("display") === "none") {
+    $(".detail-sec").removeClass("on");
+    $(".pricing-item").removeClass("on");
+    $(this).text("자세히 알아보기");
+  } else {
+    $(".detail-sec").addClass("on");
+    $(".pricing-item").addClass("on");
+    $(this).text("도입 문의하기");
+  }
+});
