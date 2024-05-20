ed23a5a387939e9e2d96ec7c49ac36efc610a52a
gyuri.son <gyuri.son@weaverloft.com>
2024-05-20 18:55:26 +0900

    feat: 반응형 작업

--- asset/js/mall-core.js
+++ asset/js/mall-core.js
@@ -2,34 +2,24 @@ document.addEventListener("DOMContentLoaded", function () {
   // introduce-sec
   document.fonts.ready.then(function () {
     const mainTitleAnimation = gsap.timeline();
-    mainTitleAnimation
-      .to($(".introduce-sec .introduce-title-area"), { xPercent: 20, opacity: 1 }, "a")
-      .to($(".introduce-sec .description-area"), { xPercent: -20, opacity: 1 }, "a");
+
+    if (window.innerWidth > 768) {
+      mainTitleAnimation
+        .to($(".introduce-sec .introduce-title-area"), { xPercent: 20, opacity: 1 }, "a")
+        .to($(".introduce-sec .description-area"), { xPercent: -20, opacity: 1 }, "a");
+    } else {
+      mainTitleAnimation
+        .to($(".introduce-sec .introduce-title-area"), { yPercent: -30, opacity: 1 })
+        .to($(".introduce-sec .description-area"), { yPercent: -30, opacity: 1 });
+    }
   });
 
-  // titleAnimation 공통
-  // 한 글자씩 자르기
+  // title 한 글자씩 자르기
   document.querySelectorAll(".title .split").forEach((element) => {
     element.innerHTML = element.textContent
       .split("")
       .map((char) => `<span>${char}</span>`)
       .join("");
-  });
-
-  document.querySelectorAll(".titleAnimation").forEach(function (section) {
-    gsap.to(section.querySelectorAll(".title .split span"), {
-      scrollTrigger: {
-        trigger: section,
-        start: "0% 50%",
-        end: "100% 0%",
-        toggleActions: "play none none reverse",
-        // markers: true,
-      },
-      stagger: 0.05,
-      color: "#15C1D8",
-      duration: 0.5,
-      ease: "power2.in",
-    });
   });
 
   // solution-sec
@@ -50,56 +40,80 @@ document.addEventListener("DOMContentLoaded", function () {
   animate();
   setInterval(animate, 4000);
 
-  gsap.from(".solution-list h3", {
+  const solutionAnimation = gsap.timeline({
     scrollTrigger: {
-      trigger: ".solution-list",
-      start: "0% 60%",
+      trigger: ".solution-sec",
+      start: "0% 50%",
       end: "100% 0%",
       toggleActions: "play none none reverse",
       // markers: true,
     },
-    "--height": 0,
-    stagger: 0.5,
   });
+  solutionAnimation
+    .to($(".solution-sec .title .split span"), {
+      stagger: 0.05,
+      color: "#15C1D8",
+      duration: 0.3,
+      ease: "power2.inOut",
+    })
+    .from($(".solution-sec .solution-list h3"), { "--height": 0, stagger: 0.3 });
 
   // benefit-sec
-  const counter = { counter: 0 };
-
-  function countAnimation(selector, value) {
-    const target = document.querySelector(selector);
-    gsap.to(counter, {
-      counter: value,
-      scrollTrigger: {
-        trigger: ".benefit-sec .list-area",
-        start: "0% 60%",
-        end: "100% 0%",
-        toggleActions: "play none none reverse",
-        // markers:  true,
-      },
-      onUpdate: () => {
-        target.innerHTML = (value > 0 ? "+" : "") + counter.counter.toFixed() + "<sup>%</sup>";
-      },
-      duration: 2,
-    });
-  }
-
-  countAnimation("#build-cost", -50);
-  countAnimation("#operation-cost", -70);
-  countAnimation("#speed", 200);
-
-  gsap.to(".benefit-sec .benefit-list li p", {
+  const benefitAnimation = gsap.timeline({
     scrollTrigger: {
-      trigger: ".benefit-sec .list-area",
-      start: "0% 60%",
+      trigger: ".benefit-sec",
+      start: "0% 50%",
       end: "100% 0%",
       toggleActions: "play none none reverse",
       // markers: true,
     },
-    "--width": "100%",
-    duration: 2,
+  });
+
+  const counters = {
+    buildCost: { selector: "#build-cost", value: 0 },
+    operationCost: { selector: "#operation-cost", value: 0 },
+    speed: { selector: "#speed", value: 0 },
+  };
+
+  benefitAnimation
+    .to($(".benefit-sec .title .split span"), {
+      stagger: 0.05,
+      color: "#15C1D8",
+      duration: 0.3,
+      ease: "power2.inOut",
+    })
+    .to($(".benefit-sec .benefit-list li p"), { "--width": "100%", duration: 2 }, "a");
+  Object.keys(counters).forEach((key) => {
+    benefitAnimation.fromTo(
+      counters[key],
+      { value: 0 },
+      {
+        value: key === "buildCost" ? -50 : key === "operationCost" ? -70 : 200,
+        onUpdate: function () {
+          const value = this.targets()[0].value.toFixed();
+          document.querySelector(counters[key].selector).innerHTML = value + "<sup>%</sup>";
+        },
+        duration: 2,
+      },
+      "a"
+    );
   });
 
   // technology-sec
+  gsap.to(".technology-sec .title .split span", {
+    scrollTrigger: {
+      trigger: ".technology-sec",
+      start: "0% 60%",
+      end: "100% 0%",
+      toggleActions: "play none none reverse",
+      // markers: true,
+    },
+    stagger: 0.05,
+    color: "#15C1D8",
+    duration: 0.3,
+    ease: "power2.inOut",
+  });
+
   const upAnimation = gsap.timeline({
     scrollTrigger: {
       trigger: ".technology-sec",
@@ -110,8 +124,9 @@ document.addEventListener("DOMContentLoaded", function () {
     },
     duration: 0.5,
   });
-  upAnimation.from($(".technology-sec .common-title-area"), { y: 100, opacity: 0 });
-  upAnimation.from($(".technology-sec .content-area"), { y: 100, opacity: 0 });
+  upAnimation
+    .from($(".technology-sec .common-title-area"), { y: 100, opacity: 0 })
+    .from($(".technology-sec .content-area"), { y: 100, opacity: 0 });
 
   // pricing-sec
   const upAnimation2 = gsap.timeline({
@@ -123,9 +138,10 @@ document.addEventListener("DOMContentLoaded", function () {
       // markers: true,
     },
   });
-  upAnimation2.from($(".pricing-sec .detail-title-area"), { y: 100, opacity: 0 });
-  upAnimation2.from($(".pricing-sec .list-area"), { y: 100, opacity: 0 });
-  upAnimation2.from($(".pricing-sec .btn-area"), { y: 100, opacity: 0 });
+  upAnimation2
+    .from($(".pricing-sec .detail-title-area"), { y: 100, opacity: 0 })
+    .from($(".pricing-sec .list-area"), { y: 100, opacity: 0 })
+    .from($(".pricing-sec .btn-area"), { y: 100, opacity: 0 });
 
   const items = $(".pricing-sec .pricing-item");
   const indexArr = [1, 2, 0];
@@ -177,20 +193,26 @@ document.addEventListener("DOMContentLoaded", function () {
     },
     ease: "power2.inOut",
   });
-  diagramAnimation.from($(".diagram-sec .detail-title-area"), { y: 100, opacity: 0 });
-  diagramAnimation.from($(".diagram-title"), { y: 100, opacity: 0 }, "a");
-  diagramAnimation.from($(".diagram-center"), { y: 100, opacity: 0 }, "a");
-  diagramAnimation.from($(".diagram-left"), { x: -100, opacity: 0 });
-  diagramAnimation.from($(".left-line #circle1"), { opacity: 0 }, "-=0.1");
-  diagramAnimation.from($(".left-line #path"), { strokeDashoffset: -81, duration: 0.5 }, "-=0.1");
-  diagramAnimation.from($(".left-line #circle2"), { opacity: 0 }, "-=0.1");
-  diagramAnimation.from($(".diagram-right"), { x: 100, opacity: 0 });
-  diagramAnimation.from($(".right-line #circle1"), { opacity: 0 }, "-=0.1");
-  diagramAnimation.from($(".right-line #path"), { strokeDashoffset: 50, duration: 0.5 }, "-=0.1");
-  diagramAnimation.from($(".right-line #circle2"), { opacity: 0 }, "-=0.1");
-  diagramAnimation.from($(".diagram-bottom"), { y: 100, opacity: 0 });
-  diagramAnimation.from($(".bottom-line #path"), { strokeDashoffset: -100, duration: 0.5 }, "-=0.1");
-  diagramAnimation.from($(".bottom-line #circle"), { opacity: 0 }, "-=0.1");
+  diagramAnimation
+    .from($(".diagram-sec .detail-title-area"), { y: 100, opacity: 0 })
+    .from($(".diagram-title"), { y: 100, opacity: 0 }, "a");
+  if (window.innerWidth > 768) {
+    diagramAnimation
+      .from($(".diagram-center"), { y: 100, opacity: 0 }, "a")
+      .from($(".diagram-left"), { x: -100, opacity: 0 })
+      .from($(".left-line #circle1"), { opacity: 0 }, "-=0.1")
+      .from($(".left-line #path"), { strokeDashoffset: -81, duration: 0.5 }, "-=0.1")
+      .from($(".left-line #circle2"), { opacity: 0 }, "-=0.1")
+      .from($(".diagram-right"), { x: 100, opacity: 0 })
+      .from($(".right-line #circle1"), { opacity: 0 }, "-=0.1")
+      .from($(".right-line #path"), { strokeDashoffset: 50, duration: 0.5 }, "-=0.1")
+      .from($(".right-line #circle2"), { opacity: 0 }, "-=0.1")
+      .from($(".diagram-bottom"), { y: 100, opacity: 0 })
+      .from($(".bottom-line #path"), { strokeDashoffset: -100, duration: 0.5 }, "-=0.1")
+      .from($(".bottom-line #circle"), { opacity: 0 }, "-=0.1");
+  } else {
+    diagramAnimation.from($(".mo-diagram"), { y: 100, opacity: 0 }, "a");
+  }
 
   gsap.from(".consulting-sec", {
     scrollTrigger: {
@@ -216,8 +238,9 @@ document.addEventListener("DOMContentLoaded", function () {
     },
     duration: 0.5,
   });
-  upAnimation3.from($(".function-sec .common-title-area"), { y: 100, opacity: 0 });
-  upAnimation3.from($(".function-sec .swiper"), { y: 100, opacity: 0 });
+  upAnimation3
+    .from($(".function-sec .common-title-area"), { y: 100, opacity: 0 })
+    .from($(".function-sec .swiper"), { y: 100, opacity: 0 });
 
   const swiper = new Swiper(".swiper", {
     slidesPerView: "auto",
